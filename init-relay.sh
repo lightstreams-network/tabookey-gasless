c302bb47eb5760728487899b6949334569c7b992
Iris Garcia <iris.garcia@bestsecret.com>
2019-09-13 15:14:58 +0200

    Joins run and init scripts into one.

--- init-relay.sh
+++ init-relay.sh
@@ -1,47 +1,53 @@
 #!/bin/bash -e
 
-echo "Configurable networks: 'standalone','sirius','mainnet'"
-
-if [ "$1" != "standalone" ] && [ "$1" != "sirius" ] && [ "$1" != "mainnet" ]; then
-    echo "Invalid network: $1"
-else
-    echo "Using network: $1"
-fi
-
-network=$1
-
-function onexit() {
-	echo onexit
-	pkill -f RelayHttpServer
-}
-
-trap onexit EXIT
-
 dir=`dirname $0`
 root=`cd $dir;pwd`
-
-cd $root
-
 gobin=$root/build/server/bin/
-export GOPATH=$root/server/:$root/build/server
-echo "Using GOPATH=" $GOPATH
 
-./scripts/extract_abi.js
-make -C server
+function init_relay() {
+    echo "Configurable networks: 'standalone','sirius','mainnet'"
 
-blocktime=${T=0}
+    if [ "$1" != "standalone" ] && [ "$1" != "sirius" ] && [ "$1" != "mainnet" ]; then
+	echo "Invalid network: $1"
+    else
+	echo "Using network: $1"
+    fi
 
-pkill -f RelayHttpServer && echo kill old relayserver
+    network=$1
 
-hubaddr=`npx truffle migrate --network=$network --reset | tee /dev/stderr | grep -A 4 "RelayHub" | grep "contract address" | grep "0x.*" -o`
+    cd $root
 
-if [ -z "$hubaddr" ]; then
-echo "FATAL: failed to detect RelayHub address"
-exit 1
-fi
+    export GOPATH=$root/server/:$root/build/server
+    echo "Using GOPATH=" $GOPATH
 
-echo $hubaddr
+    ./scripts/extract_abi.js
+    make -C server
 
-#fund relay:
-relayurl=http://localhost:8090
-( sleep 1 ; ./scripts/fundrelay.js $hubaddr $relayurl 0 )
+    blocktime=${T=0}
+
+    hubaddr=`npx truffle migrate --network=$network --reset | tee /dev/stderr | grep -A 4 "RelayHub" | grep "contract address" | grep "0x.*" -o`
+
+    if [ -z "$hubaddr" ]; then
+	echo "FATAL: failed to detect RelayHub address"
+	exit 1
+    fi
+
+    echo $hubaddr > ./hubaddr.txt
+
+    relayurl=http://localhost:8090
+    ( sleep 1 ; ./scripts/fundrelay.js $hubaddr $relayurl 0 )
+}
+
+function run_relay() {
+    hubaddr=$(cat ${root}/hubaddr.txt)
+    $gobin/RelayHttpServer -DefaultGasPrice 500000000000 -GasPricePercent 0 -RelayHubAddress $hubaddr -RegistrationBlockRate 100 -Workdir $root/build/server
+}
+
+function main() {
+    if [ ! -f $root/hubaddr.txt ]; then
+	init_relay
+    fi
+    run_relay
+}
+
+main
